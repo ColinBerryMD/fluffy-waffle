07d9b15fca412c318a5d2987d6bca6e1d096b149
ColinBerryMD <colin.berry.md@gmail.com>
2023-06-20 18:02:29 +0000

    fixed bugs in sms_client

--- sms_client.py
+++ sms_client.py
@@ -1,22 +1,20 @@
 import os
+from datetime import datetime
 from flask import Flask, Blueprint, render_template, request, url_for, flash, redirect, session
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
+from sqlalchemy.sql import func, or_
 
 from .phonenumber import cleanphone
 
 from .extensions import v_client, twilio_config, db, environ
 from .models import SMSClient
+from .auth import login_required, current_user
 
-sms_client = Blueprint('sms_client', __name__)
+sms_client = Blueprint('sms_client', __name__, url_prefix='/client')
 
-@sms_client.route('/sms_client_list')
-def sms_client_list():
-    clients = SMSClient.query.all()
-    return render_template('index.html', clients=clients)
 
-@sms_client.route('/create_sms_client', methods=('GET', 'POST'))
-def create_sms_client():
+@sms_client.route('/create', methods=('GET', 'POST'))
+def create():
     if request.method == 'POST':
         translate = False
         if request.form.get('translate'):
@@ -24,6 +22,7 @@ def create_sms_client():
         
         firstname = request.form['firstname']
         lastname  = request.form['lastname']
+        dob_str   = request.form['dob']
         email     = request.form['email']
         phone     = cleanphone(request.form['phone'])
         translate = translate
@@ -31,6 +30,7 @@ def create_sms_client():
 
         session['firstname'] = firstname 
         session['lastname']  = lastname
+        session['dob']       = dob_str
         session['email']     = email   
         session['phone']     = phone   
         session['translate'] = translate 
@@ -41,9 +41,16 @@ def create_sms_client():
             flash('One or more required fields is empty.')
             return render_template('sms_client.html')
 
-        existing_sms_client = SMSClient.query.filter_by(email = email).first() # if this returns a user, then the email already exists in database
+        # check fo valid date string
+        if not (bool(datetime.strptime(dob_str, '%m/%d/%Y'))):
+            flash('Date of birth is misformatted.','error')
+            return render_template('sms_client.html')
+
+        existing_sms_client=False  #for testing
+        #existing_sms_client = SMSClient.query.filter(or_(SMSClient.email==email, SMSClient.phone==phone)).first()
+        # if this returns a user, then the email or phone already exists in database
         if existing_sms_client : # if a user is found, we want to redirect back to signup page so user can try again
-            flash('Email address already exists')
+            flash('Client with this email or phone already exists','error')
             return render_template('sms_client.html')
         
         # send verification code from twilio to proposed sms number
@@ -59,8 +66,11 @@ def terms():
     if request.method == 'POST':
         OTP = request.form['one_time_password']
 
+        datetime_obj = datetime.strptime(session['dob'],'%m/%d/%Y')
+
         new_sms_client = SMSClient( firstname = session['firstname'], 
                                     lastname = session['lastname'],
+                                    dob = datetime_obj,
                                     email = session['email'],
                                     phone = session['phone'],
                                     translate = session['translate'],
@@ -83,13 +93,37 @@ def terms():
 
     return render_template('terms.html')
 
-@sms_client.route('/<int:sms_client_id>/edit-sms_client/', methods=('GET', 'POST'))
-def edit_sms_client(sms_client_id):
-    current_sms_client = SMSClient.query.get_or_404(sms_client_id)
+@sms_client.route('/list')
+@login_required
+def list():
+
+    # require sms access
+    if not current_user.is_sms:
+        flash('You need messaging access for this.','error')
+        return redirect(url_for('main.index'))
+
+    clients = SMSClient.query.all()
+    return render_template('client_index.html', clients=clients)
+
+@sms_client.route('/<int:sms_client_id>/edit/', methods=('GET', 'POST'))
+@login_required
+def edit(sms_client_id):
+
+    # require sms access
+    if not current_user.is_sms:
+        flash('You need messaging access for this.','error')
+        return redirect(url_for('main.index'))
+
+    client_to_edit = SMSClient.query.get_or_404(sms_client_id)
+    if client_to_edit.dob:
+        dob_str = client_to_edit.dob.strftime('%m/%d/%Y')
+    else:
+        dob_str = None
 
     if request.method == 'POST':
         firstname   = request.form.get('firstname')
         lastname    = request.form.get('lastname')
+        dob_str     = request.form.get('dob_str')
         email       = request.form.get('email')
         phone       = cleanphone(request.form.get('phone'))
         
@@ -101,40 +135,65 @@ def edit_sms_client(sms_client_id):
         # check for empty fields
         if not firstname or not lastname or not email or not phone:
             flash('One or more required fields is empty.','error')
-            return render_template('edit.html', current_sms_client=current_sms_client)
-        
-        existing_sms_client = SMSClient.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
-        if existing_sms_client : # if a user is found, we want to redirect back to signup page so user can try again
-            flash('New email address already exists', 'error')
-            return render_template('edit.html', current_sms_client=current_sms_client)
-        
-        current_sms_client.firstname = firstname
-        current_sms_client.lastname = lastname
-        current_sms_client.email = email
-        current_sms_client.phone = phone
-        current_sms_client.translate = translate
-        current_sms_client.blocked = False
+            return render_template('edit-client.html', client=client_to_edit, dob_str=dob_str)
 
-        db.session.add(current_sms_client)
+        # check fo valid date string
+        if not (bool(datetime.strptime(dob_str, '%m/%d/%Y'))) or not dob_str:
+            flash('Date of birth is misformatted or blank.','error')
+            return render_template('edit-client.html', client=client_to_edit, dob_str=dob_str)
+        else:
+            dob_obj = datetime.strptime(dob_str,'%m/%d/%Y')
+
+        existing_sms_client=False # for testing
+        #existing_sms_client = SMSClient.query.filter(or_(SMSClient.email==email, SMSClient.phone==phone)).first()
+        if existing_sms_client : # if a user is found, we want to redirect back to signup page so user can try again
+            flash('New email address or phone number already exists', 'error')
+            return render_template('edit-client.html', client=client_to_edit, dob_str = dob_str)
+        
+        
+        client_to_edit.firstname = firstname
+        client_to_edit.lastname = lastname
+        client_to_edit.dob = dob_obj
+        client_to_edit.email = email
+        client_to_edit.phone = phone
+        client_to_edit.translate = translate
+        client_to_edit.blocked = False
+
+        db.session.add(client_to_edit)
         db.session.commit()
 
+        flash('SMS client updated.','info')
         return redirect(url_for('main.index'))
 
-    return render_template('edit.html', current_sms_client=current_sms_client)
+    return render_template('edit-client.html', client=client_to_edit, dob_str= dob_str)
 
 @sms_client.post('/<int:sms_client_id>/block/')
-def sms_client_block(sms_client_id):
-    current_sms_client = SMSClient.query.get_or_404(sms_client_id)
-    current_sms_client.blocked = True
-    db.session.add(current_sms_client)
+@login_required
+def block(sms_client_id):
+
+    # require sms access
+    if not current_user.is_sms:
+        flash('You need messaging access for this.','error')
+        return redirect(url_for('main.index'))
+
+    sms_client_to_block = SMSClient.query.get_or_404(sms_client_id)
+    sms_client_to_block.blocked = True
+    db.session.add( sms_client_to_block)
     db.session.commit()
-    flash('Client blocked','info')
+    flash('Client '+sms_client_to_block.firstname +' '+sms_client_to_block.lastname+' blocked.','info')
     return redirect(url_for('main.index'))
 
 @sms_client.post('/<int:sms_client_id>/delete/')
-def sms_client_delete(sms_client_id):
-    current_sms_client = SMSClient.query.get_or_404(current_sms_client_id)
-    db.session.delete(current_sms_client)
+@login_required
+def delete(sms_client_id):
+
+    # require sms access
+    if not current_user.is_sms:
+        flash('You need messaging access for this.','error')
+        return redirect(url_for('main.index'))
+
+    sms_client_to_delete = SMSClient.query.get_or_404(sms_client_id)
+    db.session.delete(sms_client_to_delete)
     db.session.commit()
-    flash('Client deleted.','info')
+    flash('Client '+sms_client_to_delete.firstname +' '+sms_client_to_delete.lastname+' deleted.','info')
     return redirect(url_for('main.index'))
